c305b76c269cef80aa4c64e968bbf3691a8df080
CodeWu91 <453973171@qq.com>
2015-12-04 00:21:12 +0800

    modified:   Springmvc/.project 	new file:   Springmvc/WebContent/WEB-INF/springmvc-servlet.xml 	modified:   Springmvc/WebContent/WEB-INF/web.xml 	new file:   Springmvc/WebContent/hello.jsp 	new file:   Springmvc/WebContent/index.jsp 	new file:   Springmvc/src/springmvc/HelloController.java 	new file:   "Springmvc/\346\200\273\347\273\223"

---
+++ Springmvc/src/springmvc/HelloController.java
@@ -0,0 +1,22 @@
+package springmvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		String hello = request.getParameter("hello");
+		System.out.print("-----:"+hello);
+		ModelAndView mav=  new ModelAndView("index");
+		mav.addObject("helloworld", "hello "+ hello);
+		return mav;
+	}
+
+	
+}
