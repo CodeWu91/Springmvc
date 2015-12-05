f171a9fc1c511a954287e12e79483981ef87ff17
CodeWu91 <453973171@qq.com>
2015-12-05 23:28:37 +0800

    没什么

---
+++ Springmvc/src/com/liu/filter/Utf8Filter.java
@@ -0,0 +1,52 @@
+package com.liu.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class Utf8Filter
+ */
+@WebFilter("/*")
+public class Utf8Filter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Utf8Filter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		request.setCharacterEncoding("utf-8");
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
